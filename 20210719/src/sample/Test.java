e88da82e1d1c6000d2c2b7369fd53fa21e5fc468
kimshape <elves9918@gmail.com>
2021-07-19 17:36:40 +0900

    210719 수업내용 및 과제

---
+++ 20210719/src/sample/Test.java
@@ -0,0 +1,49 @@
+package sample;
+
+import java.io.FileReader;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Properties;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short a =1;
+		System.out.println(a*10);
+		//                 s  i
+		//               s-> i  * i
+		//               2   4    4 
+		//a = a * 10;
+		//s = s->i * i
+		
+		
+		int kor1 ;
+		int kor2 ;
+		int kor3 ;
+		
+		int kors[];
+		
+		//Enumeration
+		//Iterator		
+		ResultSet resultSet;
+		Hashtable<String, Date> hashtable ;
+		
+		//FileReader
+		//Properties
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
